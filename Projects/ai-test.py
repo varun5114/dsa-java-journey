162a6a18082409ff009fca0923068e23acd3e7cf
varun5114 <murajitvarun@gmail.com>
2026-03-24 21:39:40 +0530

    day 12 of practicing leet code and python

--- Projects/ai-test.py
+++ Projects/ai-test.py
@@ -1,10 +1,18 @@
 import numpy as np
-
+greater_count=0
+smaller_count=0
 
 data=np.array([1,2,3,4,5])
 print("mean: ",np.mean(data))
+for i in data:
+    if(np.mean(data)<i):
+        greater_count+=1
+    elif(np.mean(data)>i):
+        smaller_count+=1
 print("median: ",np.median(data))
 print("min: ",np.min(data))
+print("Greater_count: ",greater_count)
+print("smaller_count:",smaller_count)
 
 count=0
 nums=[]
